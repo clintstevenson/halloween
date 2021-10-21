e08b05d4d522e4f71f9969f5caf538bc8e3d5e81
Clint W Stevenson <4991992+clintstevenson@users.noreply.github.com>
2021-10-21 09:36:21 -0600

    Added comments.

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ PIR_SENSOR = 24
 def halloween():
     # Initialized the sensor, relay 1, and relay 2
     pir = gz.MotionSensor(PIR_SENSOR)
-    relay1 = gz.OutputDevice(RELAY_1, active_high=False, initial_value=False)
-    relay2 = gz.OutputDevice(RELAY_2, active_high=False, initial_value=False)
+    relay1 = gz.OutputDevice(RELAY_1, active_high = False, initial_value = False)
+    relay2 = gz.OutputDevice(RELAY_2, active_high = False, initial_value = False)
 
     logging.basicConfig(filename='halloween.log',level=logging.DEBUG)
     logging.debug('Starting Halloween')
@@ -51,14 +51,16 @@ def halloween():
                     time.sleep(7)
                     relay2.off()
 
-                time.sleep(1)
+                # Give kids 1 minute to move along so the relay doesn't constantly trigger.
+                time.sleep(60)
 
             except Exception as e:
                 logging.debug('Error: ' + str(e))
     except KeyboardInterrupt:
-            pir.close()
-            relay1.close()
-            relay2.close()
+        # For testing purposes. Manually cancel and shut down the PIR and relays.   
+        pir.close()
+        relay1.close()
+        relay2.close()
 
 
 if __name__ == '__main__':    
